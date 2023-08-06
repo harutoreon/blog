c70009e38d5349667a599ebf40d080bb09a72902
harutoreon <norabder567@gmail.com>
2023-08-06 11:04:35 +0900

    test: Created controller show test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = Article.create(title: "Rails is Awesome!", body: "It really is.", status: "public")
+  end
+
   test "should get index" do
     get articles_url
+    assert_response :success
+  end
+
+  test "should show article" do
+    get article_url(@article)
     assert_response :success
   end
 end
