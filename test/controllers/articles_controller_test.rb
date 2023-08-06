f37decfc31033da9021ffd69440e4c50f7cf4129
harutoreon <norabder567@gmail.com>
2023-08-06 11:37:40 +0900

    test: Created controller test new

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -14,4 +14,9 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     get article_url(@article)
     assert_response :success
   end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
 end
