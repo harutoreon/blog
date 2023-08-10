c56071dfe5c60053ca9d882f0f6845fde65b8435
harutoreon <norabder567@gmail.com>
2023-08-11 04:20:48 +0900

    test: Created system test article

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -5,4 +5,17 @@ class ArticlesTest < ApplicationSystemTestCase
     visit root_url
     assert_selector "h1", text: "Articles"
   end
+
+  test "should create article" do
+    visit root_url
+
+    click_on "New Article"
+
+    fill_in "Title", with: "Creating an Article"
+    fill_in "Body", with: "Created this article successfully!"
+
+    click_on "Create Article"
+
+    assert_text "Creating an Article"
+  end
 end
